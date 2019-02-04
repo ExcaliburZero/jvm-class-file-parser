89b0b0023f6efbee189c9d696aa51566723f54dd
Christopher Wells <cwellsny@nycap.rr.com>
2019-02-04 16:10:47 -0500

    Add example to module documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,17 @@
 #![feature(try_from)]
+//! This is a (partially implemented) Rust library and program for parsing JVM class files.
+//!
+//! ```
+//! extern crate jvm_class_file_parser;
+//! 
+//! use std::fs::File;
+//! use jvm_class_file_parser::ClassFile;
+//! 
+//! let mut file = File::open("classes/Dummy.class").unwrap();
+//! let class_file = ClassFile::from_file(&mut file).unwrap();
+//! 
+//! assert_eq!("Dummy", class_file.get_class_name());
+//! ```
 
 mod attribute;
 mod bytecode;
