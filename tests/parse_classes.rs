6ca1de258dd8a2f94e68e8776692415f0644757f
Christopher Wells <cwellsny@nycap.rr.com>
2019-02-04 18:05:43 -0500

    Add more assertions to IntBox test case

--- tests/parse_classes.rs
+++ tests/parse_classes.rs
@@ -37,4 +37,49 @@ fn parse_class_dummy() {
 fn parse_class_intbox() {
     let mut file = File::open("classes/IntBox.class").unwrap();
     let class_file = ClassFile::from_file(&mut file).unwrap();
+
+    assert_eq!("IntBox", class_file.get_class_name());
+
+    assert_eq!(Some("IntBox.java"), class_file.get_source_file_name());
+
+    assert_eq!(2, class_file.methods.len());
+
+    let constructor = &class_file.methods[0];
+    let constructor_code = constructor.get_code(&class_file);
+
+    use Bytecode::*;
+    assert_eq!(
+        Some(Code {
+            max_stack: 2,
+            max_locals: 2,
+            code: vec![
+                (0, Aload_0),
+                (1, Invokespecial(1)),
+                (4, Aload_0),
+                (5, Iload_1),
+                (6, Putfield(2)),
+                (9, Return),
+            ],
+            exception_table: vec![],
+        }),
+        constructor_code
+    );
+
+    let get_value = &class_file.methods[1];
+    let get_value_code = get_value.get_code(&class_file);
+
+    use Bytecode::*;
+    assert_eq!(
+        Some(Code {
+            max_stack: 1,
+            max_locals: 1,
+            code: vec![
+                (0, Aload_0),
+                (1, Getfield(2)),
+                (4, Ireturn),
+            ],
+            exception_table: vec![],
+        }),
+       get_value_code
+    );
 }
