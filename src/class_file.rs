f3c398c990b1d9f34f496355c38bc0647c1cffce
Christopher Wells <cwellsny@nycap.rr.com>
2019-02-03 19:58:53 -0500

    Add method to get the name of a class

--- src/class_file.rs
+++ src/class_file.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io;
+use std::ops::Deref;
 
 use attribute::*;
 use constant_pool::*;
@@ -24,5 +25,25 @@ pub struct ClassFile {
 impl ClassFile {
     pub fn from_file(file: &mut File) -> io::Result<ClassFile> {
         parsing::read_class_file(file)
+    }
+
+    pub fn get_class_name(&self) -> &str {
+        let class = self.get_constant(self.this_class as usize);
+
+        match class.deref() {
+            &ConstantPoolEntry::ConstantClass { name_index } => {
+                let class_name = self.get_constant(name_index as usize);
+
+                match class_name.deref() {
+                    &ConstantPoolEntry::ConstantUtf8 { ref string } => string,
+                    _ => panic!(),
+                }
+            },
+            _ => panic!(),
+        }
+    }
+
+    fn get_constant(&self, index: usize) -> &Box<ConstantPoolEntry> {
+        &self.constant_pool[index - 1]
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ use jvm_class_file_parser::ClassFile;
 fn main() {
     let mut file = File::open("Test.class").unwrap();
 
-    let class_file = ClassFile::from_file(&mut file);
+    let class_file = ClassFile::from_file(&mut file).unwrap();
 
     println!("{:#?}", class_file);
+
+    println!("{}", class_file.get_class_name());
 }
