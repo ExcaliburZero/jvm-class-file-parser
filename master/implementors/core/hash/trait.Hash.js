d3e4bf7458e7ce33161bfacd29c6f9222e3ff418
Travis CI User <travis@example.org>
2019-02-10 22:58:32 +0000

    Automatic Travis documentation build

--- master/implementors/core/hash/trait.Hash.js
+++ master/implementors/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["jvm_class_file_parser"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"jvm_class_file_parser/enum.ClassAccess.html\" title=\"enum jvm_class_file_parser::ClassAccess\">ClassAccess</a>",synthetic:false,types:["jvm_class_file_parser::class_access::ClassAccess"]},];
+implementors["jvm_class_file_parser"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"jvm_class_file_parser/enum.ClassAccess.html\" title=\"enum jvm_class_file_parser::ClassAccess\">ClassAccess</a>",synthetic:false,types:["jvm_class_file_parser::class_access::ClassAccess"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"jvm_class_file_parser/enum.FieldAccess.html\" title=\"enum jvm_class_file_parser::FieldAccess\">FieldAccess</a>",synthetic:false,types:["jvm_class_file_parser::field_access::FieldAccess"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
