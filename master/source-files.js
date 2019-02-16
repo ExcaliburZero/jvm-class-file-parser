b56e55a6a943aae408bfe4ba9e40219bd122338a
Travis CI User <travis@example.org>
2019-02-16 18:16:58 +0000

    Automatic Travis documentation build

--- master/source-files.js
+++ master/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["jvm_class_file_parser"] = {"name":"","dirs":[],"files":["attribute.rs","bytecode.rs","class_access.rs","class_file.rs","constant_pool.rs","field.rs","field_access.rs","lib.rs","method.rs","parsing.rs","util.rs"]};
+sourcesIndex["jvm_class_file_parser"] = {"name":"","dirs":[],"files":["attribute.rs","bytecode.rs","class_access.rs","class_file.rs","constant_pool.rs","field.rs","field_access.rs","lib.rs","method.rs","parsing.rs","util.rs","writing.rs"]};
 createSourceSidebar();
